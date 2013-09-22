c9f1e9501be975da7f87cb96c9448ce78fffbe86
Renan Gemignani <renan.gemignani@gmail.com>
2013-09-22 11:09:18 -0300

    Functional canvas, server-side views and client-side socketing.
    Still missing everything for the update pusher's.

---
+++ server/public/scripts/canvas_client.js
@@ -0,0 +1,196 @@
+var canvas = $('#main_canvas');
+
+var geometry = {
+    width: 400.,
+    length: 600.,
+    border: 25.,
+    line_width: 2.,
+    center_radius: 50.,
+    defense_radius: 50.,
+    defense_stretch: 35.,
+    goal_width: 70.,
+    goal_depth: 18.,
+    robot_radius: 15,
+};
+
+var colors = {
+    dark_green: "#008000",
+    yellow: "#FFFF00",
+    blue: "#0000FF",
+    white: "#FFFFFF",
+    black: "#000000",
+};
+
+var robots = []
+
+function drawField()
+{
+    // Draw green on field
+    canvas.drawRect({
+            fillStyle: colors.dark_green,
+            x: 0, 
+            y: 0, 
+            width: geometry.length + 2 * geometry.border, 
+            height: geometry.width + 2 * geometry.border,
+            fromCenter: false,
+        }
+    ); 
+    
+    // Draw outside perimeter
+    canvas.drawRect({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x: geometry.border, 
+            y: geometry.border, 
+            width: geometry.length, 
+            height: geometry.width,
+            fromCenter: false,
+        }
+    ); 
+
+    // Draw midball
+    canvas.drawArc({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x: geometry.border + geometry.length / 2, 
+            y: geometry.border + geometry.width / 2, 
+            radius: geometry.center_radius
+        }
+    );
+   
+    // Draw midline
+    canvas.drawLine({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x1: geometry.border + geometry.length / 2, 
+            y1: geometry.border, 
+            x2: geometry.border + geometry.length / 2, 
+            y2: geometry.border + geometry.width, 
+        }
+    );
+
+    // Draw left defense area
+    canvas.drawArc({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x: geometry.border, 
+            y: geometry.border + geometry.width / 2 + geometry.defense_stretch / 2, 
+            radius: geometry.defense_radius,
+            start: 90,
+            end: 180,
+        }
+    );
+    canvas.drawArc({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x: geometry.border, 
+            y: geometry.border + geometry.width / 2 - geometry.defense_stretch / 2, 
+            radius: geometry.defense_radius,
+            start: 0,
+            end: 90,
+        }
+    );
+    canvas.drawLine({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x1: geometry.border + geometry.defense_radius, 
+            y1: geometry.border + geometry.width / 2 - geometry.defense_stretch / 2, 
+            x2: geometry.border + geometry.defense_radius, 
+            y2: geometry.border + geometry.width / 2 + geometry.defense_stretch / 2, 
+        }
+    );
+    // Draw right defense area
+    canvas.drawArc({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x: geometry.length + geometry.border, 
+            y: geometry.border + geometry.width / 2 + geometry.defense_stretch / 2, 
+            radius: geometry.defense_radius,
+            start: 180,
+            end: 270,
+        }
+    );
+    canvas.drawArc({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x: geometry.length + geometry.border, 
+            y: geometry.border + geometry.width / 2 - geometry.defense_stretch / 2, 
+            radius: geometry.defense_radius,
+            start: 270,
+            end: 0,
+        }
+    );
+    canvas.drawLine({
+            strokeStyle: colors.white,
+            strokeWidth: geometry.line_width,
+            x1: geometry.length + geometry.border - geometry.defense_radius, 
+            y1: geometry.border + geometry.width / 2 - geometry.defense_stretch / 2, 
+            x2: geometry.length + geometry.border - geometry.defense_radius, 
+            y2: geometry.border + geometry.width / 2 + geometry.defense_stretch / 2, 
+        }
+    );
+    //draw left goal
+    canvas.drawLine({
+            // Todo: change this
+            strokeStyle: colors.yellow,
+            strokeWidth: geometry.line_width,
+            x1: geometry.border, 
+            y1: geometry.border + geometry.width / 2 - geometry.goal_width / 2, 
+            x2: geometry.border - geometry.goal_depth, 
+            y2: geometry.border + geometry.width / 2 - geometry.goal_width / 2,
+            x3: geometry.border - geometry.goal_depth, 
+            y3: geometry.border + geometry.width / 2 + geometry.goal_width / 2,
+            x4: geometry.border, 
+            y4: geometry.border + geometry.width / 2 + geometry.goal_width / 2, 
+        }
+    );
+    //draw right goal
+    canvas.drawLine({
+            // Todo: change this
+            strokeStyle: colors.blue,
+            strokeWidth: geometry.line_width,
+            x1: geometry.length + geometry.border, 
+            y1: geometry.border + geometry.width / 2 - geometry.goal_width / 2, 
+            x2: geometry.length + geometry.border + geometry.goal_depth, 
+            y2: geometry.border + geometry.width / 2 - geometry.goal_width / 2,
+            x3: geometry.length + geometry.border + geometry.goal_depth, 
+            y3: geometry.border + geometry.width / 2 + geometry.goal_width / 2,
+            x4: geometry.length + geometry.border, 
+            y4: geometry.border + geometry.width / 2 + geometry.goal_width / 2, 
+        }
+    );
+
+}
+
+function drawRobots(robots)
+{
+    robots.forEach(
+        function(robot) {
+            canvas.drawArc({
+                    // Todo: change this
+                    fillStyle: robot.color,
+                    x: robot.x, 
+                    y: robot.y,
+                    radius: geometry.robot_radius,
+                }
+            );
+        }
+    );
+}
+
+// Run once DOM is ready.
+$(function () {
+    var socket = io.connect();
+
+    canvas = $('#main_canvas');
+    drawField();
+    socket.on(
+        'ssl_packet',
+        function(data) {
+            drawField();
+            drawRobots(data.robots);
+        }
+    );
+
+    //socket.emit('ssl_packet', {robots:[ {x: 10, y: 20, color: colors.blue}]});
+});
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var url = require('url');
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
@@ -27,17 +27,14 @@ var io = require('socket.io').listen(app.listen(80));
 io.sockets.on(
     'connection', 
     function(socket) {
-        socket.emit('conn_ack', {message: 'acknowledged'});
         socket.on(
-            'new_game', 
-            function(game_name) {
+            'ssl_packet',
+            // Right now, we only have one game per instance, so all 
+            // connected sockets get to see it.
 
-            }
-        
-        )
-        socket.on(
-            'ssl_packet', 
+            // TODO: Implement separation of sockets per game
             function(packet) {
+                console.log(packet);
                 // Forward packet to all the client sockets.
                 io.sockets.emit('ssl_packet', packet);
             }
